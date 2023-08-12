76fe0e7e8e9e47f60ed60fe928b55d176541fb8c
kaito0415 <yuitokito@docomo.ne.jp>
2023-08-12 21:51:11 +0900

    追加：時間割画面（ホーム画面）の作成をする

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,7 +12,134 @@
             
         </x-slot>
         <body>
-            
+            <div class="timetable">
+                <div class="monday_lecture">
+                    <h1>月曜日</h1>
+                    <div class="first_period">
+                        <h3>１時間目</h3>
+                    </div>
+                    <div class="second_period">
+                        <h3>２時間目</h3>
+                    </div>
+                    <div class="third_period">
+                        <h3>３時間目</h3>
+                    </div>
+                    <div class="fourth_period">
+                        <h3>４時間目</h3>
+                    </div>
+                    <div class="fifth_period">
+                        <h3>５時間目</h3>
+                    </div>
+                    <div class="sixth_period">
+                        <h3>６時間目</h3>
+                    </div>
+                </div>
+                <div class="tuesday_lecture">
+                    <h1>火曜日</h1>
+                    <div class="first_period">
+                        <h3>１時間目</h3>
+                    </div>
+                    <div class="second_period">
+                        <h3>２時間目</h3>
+                    </div>
+                    <div class="third_period">
+                        <h3>３時間目</h3>
+                    </div>
+                    <div class="fourth_period">
+                        <h3>４時間目</h3>
+                    </div>
+                    <div class="fifth_period">
+                        <h3>５時間目</h3>
+                    </div>
+                    <div class="sixth_period">
+                        <h3>６時間目</h3>
+                    </div>
+                </div>
+                <div class="wednesday_lecture">
+                    <h1>水曜日</h1>
+                    <div class="first_period">
+                        <h3>１時間目</h3>
+                    </div>
+                    <div class="second_period">
+                        <h3>２時間目</h3>
+                    </div>
+                    <div class="third_period">
+                        <h3>３時間目</h3>
+                    </div>
+                    <div class="fourth_period">
+                        <h3>４時間目</h3>
+                    </div>
+                    <div class="fifth_period">
+                        <h3>５時間目</h3>
+                    </div>
+                    <div class="sixth_period">
+                        <h3>６時間目</h3>
+                    </div>
+                </div>
+                <div class="thursday_lecture">
+                    <h1>木曜日</h1>
+                    <div class="first_period">
+                        <h3>１時間目</h3>
+                    </div>
+                    <div class="second_period">
+                        <h3>２時間目</h3>
+                    </div>
+                    <div class="third_period">
+                        <h3>３時間目</h3>
+                    </div>
+                    <div class="fourth_period">
+                        <h3>４時間目</h3>
+                    </div>
+                    <div class="fifth_period">
+                        <h3>５時間目</h3>
+                    </div>
+                    <div class="sixth_period">
+                        <h3>６時間目</h3>
+                    </div>
+                </div>
+                <div class="friday_lecture">
+                    <h1>金曜日</h1>
+                    <div class="first_period">
+                        <h3>１時間目</h3>
+                    </div>
+                    <div class="second_period">
+                        <h3>２時間目</h3>
+                    </div>
+                    <div class="third_period">
+                        <h3>３時間目</h3>
+                    </div>
+                    <div class="fourth_period">
+                        <h3>４時間目</h3>
+                    </div>
+                    <div class="fifth_period">
+                        <h3>５時間目</h3>
+                    </div>
+                    <div class="sixth_period">
+                        <h3>６時間目</h3>
+                    </div>
+                </div>
+                <div class="saturday_lecture">
+                    <h1>土曜日</h1>
+                    <div class="first_period">
+                        <h3>１時間目</h3>
+                    </div>
+                    <div class="second_period">
+                        <h3>２時間目</h3>
+                    </div>
+                    <div class="third_period">
+                        <h3>３時間目</h3>
+                    </div>
+                    <div class="fourth_period">
+                        <h3>４時間目</h3>
+                    </div>
+                    <div class="fifth_period">
+                        <h3>５時間目</h3>
+                    </div>
+                    <div class="sixth_period">
+                        <h3>６時間目</h3>
+                    </div>
+                </div>
+            </div>
         </body>
     </x-app-layout>
 </html>
