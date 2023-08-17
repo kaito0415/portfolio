2aabe26041bc6bf40fd2565570d7e5691a3f8b2d
kaito0415 <yuitokito@docomo.ne.jp>
2023-08-18 04:03:42 +0900

    修正：時間割画面の表示方法及び時間割追加画面に遷移時にパラメータを送るように修正をする

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,29 +1,3 @@
-<?php
-
-$timetable = '';
-
-for($period = 1; $period < 7; $period++){
-    $timetable .= '<tr><th class="period">' . $period . '時間目</th>';
-    
-    for($day_of_week = 0; $day_of_week < 6; $day_of_week++){
-        
-        foreach($user->lectures as $lecture){
-        
-            if($lecture->day_of_week  == $day_of_week && $lecture->period == $period){
-                $timetable .= '<th><a href="/lectures/' . $lecture->id . '/detail">' . $lecture->name . '</a><br></br><p>' . $lecture->classroom . '</p></th>';
-                break;
-            }
-            
-        }
-        
-        $timetable .= '<th></th>';
-        
-    }
-    $timetable .= '</tr>';
-
-}
-
-?>
 <!DOCTYPE html>
 <html>
     
@@ -52,12 +26,158 @@ for($period = 1; $period < 7; $period++){
                         </tr>
                     </div>
                     <div class="timetable">
-                        <?php
-                            echo $timetable;
-                        ?>
+                        <div class="first_period">
+                            <tr>
+                                <th>１時間目</th>
+                                @for($i = 0; $i < 6; $i++)
+                                    @forelse($first_lectures as $first_lecture)
+                                        @if($first_lecture->day_of_week == $i)
+                                            <th>
+                                                <a href="/lectures/{{ $first_lecture->id }}/detail">{{ $first_lecture->name }}</a>
+                                                <br/>
+                                                <p>{{ $first_lecture->classroom }}</p>
+                                            </th>
+                                            @break
+                                        @elseif($loop->last)
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=1&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                        @endif
+                                    @empty
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=1&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                    @endforelse
+                                @endfor
+                            </tr>
+                        </div>
+                        <div class="first_period">
+                            <tr>
+                                <th>２時間目</th>
+                                @for($i = 0; $i < 6; $i++)
+                                    @forelse($second_lectures as $second_lecture)
+                                        @if($second_lecture->day_of_week == $i)
+                                            <th>
+                                                <a href="/lectures/{{ $second_lecture->id }}/detail">{{ $second_lecture->name }}</a>
+                                                <br/>
+                                                <p>{{ $second_lecture->classroom }}</p>
+                                            </th>
+                                            @break
+                                        @elseif($loop->last)
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=2&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                        @endif
+                                    @empty
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=2&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                    @endforelse
+                                @endfor
+                            </tr>
+                        </div>
+                        <div class="first_period">
+                            <tr>
+                                <th>３時間目</th>
+                                @for($i = 0; $i < 6; $i++)
+                                    @forelse($third_lectures as $third_lecture)
+                                        @if($third_lecture->day_of_week == $i)
+                                            <th>
+                                                <a href="/lectures/{{ $third_lecture->id }}/detail">{{ $third_lecture->name }}</a>
+                                                <br/>
+                                                <p>{{ $third_lecture->classroom }}</p>
+                                            </th>
+                                            @break
+                                        @elseif($loop->last)
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=3&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                        @endif
+                                    @empty
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=3&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                    @endforelse
+                                @endfor
+                            </tr>
+                        </div>
+                        <div class="first_period">
+                            <tr>
+                                <th>４時間目</th>
+                                @for($i = 0; $i < 6; $i++)
+                                    @forelse($fourth_lectures as $fourth_lecture)
+                                        @if($fourth_lecture->day_of_week == $i)
+                                            <th>
+                                                <a href="/lectures/{{ $fourth_lecture->id }}/detail">{{ $fourth_lecture->name }}</a>
+                                                <br/>
+                                                <p>{{ $fourth_lecture->classroom }}</p>
+                                            </th>
+                                            @break
+                                        @elseif($loop->last)
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=4&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                        @endif
+                                    @empty
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=4&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                    @endforelse
+                                @endfor
+                            </tr>
+                        </div>
+                        <div class="first_period">
+                            <tr>
+                                <th>５時間目</th>
+                                @for($i = 0; $i < 6; $i++)
+                                    @forelse($fifth_lectures as $fifth_lecture)
+                                        @if($fifth_lecture->day_of_week == $i)
+                                            <th>
+                                                <a href="/lectures/{{ $fifth_lecture->id }}/detail">{{ $fifth_lecture->name }}</a>
+                                                <br/>
+                                                <p>{{ $fifth_lecture->classroom }}</p>
+                                            </th>
+                                            @break
+                                        @elseif($loop->last)
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=5&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                        @endif
+                                    @empty
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=5&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                    @endforelse
+                                @endfor
+                            </tr>
+                        </div>
+                        <div class="first_period">
+                            <tr>
+                                <th>６時間目</th>
+                                @for($i = 0; $i < 6; $i++)
+                                    @forelse($sixth_lectures as $sixth_lecture)
+                                        @if($sixth_lecture->day_of_week == $i)
+                                            <th>
+                                                <a href="/lectures/{{ $sixth_lecture->id }}/detail">{{ $sixth_lecture->name }}</a>
+                                                <br/>
+                                                <p>{{ $sixth_lecture->classroom }}</p>
+                                            </th>
+                                            @break
+                                        @elseif($loop->last)
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=6&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                        @endif
+                                    @empty
+                                        <th>
+                                            <a href="/lectures/add/{{ $user->id }}?p=6&d={{ $i }}">授業の追加</a>
+                                        </th>
+                                    @endforelse
+                                @endfor
+                            </tr>
+                        </div>
                     </div>
                 </table>
-                <a href="/lectures/add/{{ $user->id }}">授業の追加</a>
             </div>    
         </body>
     </x-app-layout>
