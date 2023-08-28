f48b9469c19e968ad926f8ab324783c0940c116f
kaito0415 <yuitokito@docomo.ne.jp>
2023-08-29 04:32:49 +0900

    追加：授業詳細画面のレイアウトを実装する

--- resources/views/lectures/detail.blade.php
+++ resources/views/lectures/detail.blade.php
@@ -13,32 +13,37 @@
             <h1 class="time_of_week">{{ $day_of_week }}{{ $lecture->period }}時間目</h1>
         </x-slot>
         <body>
-            <h1 class="name">
-                {{ $lecture->name }}
-            </h1>
-            <div class="detail">
-                <div class="detail_classroom">
-                    <p>{{ $lecture->classroom }}</p>
+            <div class="detail" class="card w-96 text-primary-content">
+                <div class="card-body">
+                    <h2 class="card-title">{{ $lecture->name }}</h2>
+                    <div class="detail_classroom">
+                        <p>{{ $lecture->classroom }}</p>
+                    </div>
+                    <div class="detail_teacher">
+                        <p>{{ $lecture->teacher }}</p>
+                    </div>
+                    <div class="detail_credit">
+                        <p>{{ $lecture->credit }}単位</p>
+                    </div>
+                    <div class="detail_id">
+                        <p>授業ID：{{ $lecture->id }}</p>
+                    </div>
+                    <div class="card-actions justify-end">
+                        <div class="join join-vertical">
+                            <div class="edit">
+                                <a href="/lectures/{{ $lecture->id }}/edit?user={{ $_GET['user'] }}&p={{ $lecture->period }}&d={{ $lecture->day_of_week }}" class="lecture_edit">授業内容の編集</a>
+                            </div>
+                            <br>
+                            <div class="delete">
+                                <form action="/lectures/{{ $lecture->id }}?user={{ $_GET['user'] }}" id="form_{{ $lecture->id }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button onclick="deleteLecture({{ $lecture->id }})">授業の削除</button> 
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="detail_teacher">
-                    <p>{{ $lecture->teacher }}</p>
-                </div>
-                <div class="detail_credit">
-                    <p>{{ $lecture->credit }}単位</p>
-                </div>
-                <div class="detail_id">
-                    <p>授業ID：{{ $lecture->id }}</p>
-                </div>
-            </div>
-            <div class="edit">
-                <a href="/lectures/{{ $lecture->id }}/edit?user={{ $_GET['user'] }}&p={{ $lecture->period }}&d={{ $lecture->day_of_week }}" class="lecture_edit">授業内容の編集</a>
-            </div>
-            <div class="delete">
-                <form action="/lectures/{{ $lecture->id }}?user={{ $_GET['user'] }}" id="form_{{ $lecture->id }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button onclick="deleteLecture({{ $lecture->id }})">授業の削除</button> 
-                </form>
             </div>
         </body>
     </x-app-layout>
