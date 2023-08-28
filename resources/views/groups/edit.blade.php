5ed5e5cc53babb098d46dc86ae4f91575509c044
kaito0415 <yuitokito@docomo.ne.jp>
2023-08-28 17:28:37 +0900

    追加：グループ作成画面、編集画面および参加画面のレイアウトの実装をする

--- resources/views/groups/edit.blade.php
+++ resources/views/groups/edit.blade.php
@@ -3,16 +3,15 @@
     
     <head>
         <meta charset="UTF-8">
-        <title>グループ名編集</title>
+        <title>グループ編集</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     
     <x-app-layout>
         <x-slot name="header">
-            
+            <h1 class="title">グループ情報の編集</h1>
         </x-slot>
         <body>
-            <h1 class="title">グループ名変更</h1>
             <form action="/groups/{{ $group->id }}/{{ $user->id }}" method="POST">
                 @csrf
                 @method('PUT')
@@ -20,9 +19,9 @@
                     <h3>グループ名</h3>
                     <div class="insert_name">
                         @if(!empty(old('group.name')))
-                        <input type="text" name="group[name]" placeholder="グループ名" value="{{ old('group.name') }}" />
+                        <input type="text" name="group[name]" placeholder="グループ名" value="{{ old('group.name') }}" class="input input-bordered input-info w-full max-w-xs" />
                         @else
-                        <input type="text" name="group[name]" placeholder="グループ名" value="{{ $group->name }}" />
+                        <input type="text" name="group[name]" placeholder="グループ名" value="{{ $group->name }}" class="input input-bordered input-info w-full max-w-xs" />
                         @endif
                         <p class="name_error">{{ $errors->first('group.name') }}</p>
                     </div>
@@ -31,15 +30,15 @@
                     <h3>パスワードを編集</h3>
                     <div class="insert_password">
                         @if(!empty(old('group.password')))
-                        <input type="text" name="group[password]" placeholder="パスワード" value="{{ old('group.password') }}" />
+                        <input type="text" name="group[password]" placeholder="パスワード" value="{{ old('group.password') }}" class="input input-bordered input-info w-full max-w-xs" />
                         @else
-                        <input type="text" name="group[password]" placeholder="パスワード" value="{{ $group->password }}" />
+                        <input type="text" name="group[password]" placeholder="パスワード" value="{{ $group->password }}" class="input input-bordered input-info w-full max-w-xs" />
                         @endif
                         <p class="password_error">{{ $errors->first('group.password') }}</p>
                     </div>
                 </div>
                 <div class="submit_button">
-                    <button value="submit">保存</button>
+                    <button value="submit" class="btn btn-info btn-sm">保存</button>
                 </div>
             </form>
         </body>
